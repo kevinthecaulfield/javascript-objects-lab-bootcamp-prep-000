0f5bd05e787cbf1415c2fa4fd159eb56258bcd59
Kevin Smith <kevincaulfield630@gmail.com>
2017-11-07 00:09:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object.key = 'value'
   return object
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
